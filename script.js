c1937e4ac2a092f2360a0f50e7d6e630290a8d8a
Luca Poe <80789505+iamthepoe@users.noreply.github.com>
2022-06-11 13:48:08 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-var timer = 60;
+var time = 60;
+var timer = document.querySelector('#time');
+
+function VerifyTime(){
+  if(time<=0){
+    clearInterval(myTimer);
+    timer.innerHTML = "Finalizado!";
+  }else{
+    time--;
+    timer.innerHTML = time;
+  }
+}
+
+const myTimer = setInterval(VerifyTime, '1000');
