5bb708ae2a0c9e110a9502ade1aed4ce778c17bf
Luca Poe <80789505+iamthepoe@users.noreply.github.com>
2022-06-11 17:13:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-var time = 2;
+var time = 60;
 var points = 0;
 var result = 0;
+const button = document.querySelector('#sendBtn');
+const pointsInput = document.querySelector('#points');
 const timer = document.querySelector('#time');
 const account = document.querySelector('#account');
 const answer = document.querySelector('#answer');
-const operations = ["+", "-", "*", "/"];
+const operations = ["+", "-", "*"];
 
 function VerifyTime(){
   if(time<=0){
     clearInterval(myTimer);
     timer.innerHTML = "Finalizado!";
+    answer.placeholder = "Fim de tempo!";
+    account.innerHTML = `<h3>Sua pontuação foi de:</h3><br><p style="color:#73ff00;">${points}</p>`;
     answer.readOnly = true;
-    answer.placeholder = "Fim de tempo!"
-    account.innerHTML = `<h3>Sua pontuação foi de:</h3><br><p>${points}</p>`
+    button.hidden = false;
+    pointsInput.value = points;
+    
+    document.getElementsByTagName('button')[0].hidden = false;
   }else{
     time--;
     timer.innerHTML = time;
@@ -39,12 +45,9 @@ function CreateNewAccount(){
   }else if(ChooseOperation()=="-"){
     account.innerHTML = `${x} - ${y}`
     return x - y;
-  }else if(ChooseOperation()=="*"){
+  }else{
     account.innerHTML = `${x} * ${y}`
     return x * y;
-  }else{
-    account.innerHTML = `${x} / ${y}`
-    return x / y;
   }
 }
 
